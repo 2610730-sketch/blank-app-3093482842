d37124030cf6886497bfa03e67c5968006176a4d
2610730-sketch <2610730@sh.re.kr>
2026-06-05 03:31:48 +0000

    maindd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 height=int(input('키:'))
 if height < 100:
     print('탐승불가')
